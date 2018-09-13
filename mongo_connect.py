215a478a06db75b6ba6faf0d51cb9dab0c1c5668
Aviral kumar <aviralkumar@Avirals-MacBook-Pro.local>
2018-09-12 22:52:18 -0700

    test

--- mongo_connect.py
+++ mongo_connect.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME']= 'mydb'
-app.config['MONGO_URI']='mongodb://avi:qwerty1@ds137370.mlab.com:37370/mydb'
+#app.config['MONGO_DBNAME']= 'mydb'
+#pp.config['MONGO_URI']='mongodb://avi:qwerty1@ds137370.mlab.com:37370/mydb'
 
-mongo = PyMongo(app)
+#mongo = PyMongo(app)
 
 @app.route('/add')
 
